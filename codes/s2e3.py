1640f738e74d2eb12a1223d6e7fabc0dde94ef26
IRoyalX <imreza10@gmail.com>
2024-07-13 11:38:51 +0330

    - s2e3 bug fix & report update

--- codes/s2e3.py
+++ codes/s2e3.py
@@ -1,37 +1,30 @@
-from multiprocessing import Queue
 import multiprocessing
 import time, sys
 
-def func(output):
+def func():
     name = multiprocessing.current_process().name
-    output.put(f"Starting {name}\n")
+    print (f"Starting {name}\n")
     time.sleep(0.1)
     if name == 'background_process':
         for i in range(0, 10, 2):
-            output.put(f"---> {i}\n")
+            print(f"---> {i}\n")
             time.sleep(0.2)
     else:
         for i in range(1, 10, 2):
-            output.put(f"---> {i}\n")
+            print(f"---> {i}\n")
             time.sleep(0.2)
     time.sleep(0.1)
-    output.put(f"Exiting {name}\n")
+    print (f"Exiting {name}\n")
 
 if __name__ == "__main__":
     scenario = int(sys.argv[1])
-    output = Queue()
 
-    background_process = multiprocessing.Process(name="background_process", target=func, args=(output, ))
+    background_process = multiprocessing.Process(name="background_process",target=func)
     if scenario != 2:
         background_process.daemon = True
-    NO_background_process = multiprocessing.Process(name="NO_background_process", target=func, args=(output, ))
+    NO_background_process = multiprocessing.Process(name="NO_background_process",target=func)
     NO_background_process.daemon = False
     background_process.start()
     if scenario == 3:
         background_process.join()
     NO_background_process.start()
-
-    background_process.join()
-    NO_background_process.join()
-    while not output.empty():
-        print(output.get())
